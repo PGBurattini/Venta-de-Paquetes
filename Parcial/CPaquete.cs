18757988329e12ee57b59edcac810efd0886f7b9
Patricio Gabriel Burattini <53668239+PatricioGabriel@users.noreply.github.com>
2019-10-19 10:46:50 -0300

    Add files via upload

---
+++ Parcial/CPaquete.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial
+{
+    class CPaquete
+    {
+        //Variables Miembro
+        private static float IMPUESTO;
+        private int Paquete;
+        private string Detalle;
+        private float Precio;
+        private short Cuotas;
+        private float SubPrecio;
+        
+        //Metodos
+
+        public static void setIMPUESTO(float Imp)
+        {
+            CPaquete.IMPUESTO = Imp;
+        }
+
+        public CPaquete()
+        {
+            this.Paquete = 0;
+            this.Detalle = "Sin Asignar.";
+        }
+
+        public CPaquete(int NUM, string DET)
+        {
+
+            this.Paquete = NUM;
+            this.Detalle = DET;
+        }
+
+        public void setPrecio(float monto)
+        {
+            this.Precio = monto;
+        }
+
+        public float getPrecio()
+        {
+            return this.Precio;
+        }
+
+        public float darMontoTotal()
+        {
+            float SubTotal;
+            SubTotal = (CPaquete.IMPUESTO * Precio) / 100;
+            SubTotal = SubTotal + Precio;
+
+            return SubTotal;
+
+        }
+
+        public void setCuotas(short CUO)
+        { this.Cuotas = CUO;
+        }
+
+        public float darMontoTotal(short Cuotas)
+        {
+
+            float SubTotal;
+            if (Cuotas == 1)
+
+                return this.SubPrecio;
+            else
+            {
+                SubTotal = ((Cuotas * 10) * Precio) / 100;
+                return this.SubPrecio += SubTotal;
+            }
+        }
+
+        public override string ToString()
+        {
+            string CAD;
+            CAD = "Numero de paquete: " + this.Paquete.ToString();
+            CAD += " - Detalle: " + this.Detalle;
+            CAD += " - Precio: " + this.Precio.ToString();
+            CAD += " - Impuesto: " + CPaquete.IMPUESTO.ToString();
+            CAD += " - Precio total en  " + this.Cuotas.ToString();
+            CAD += " Cuotas: " + (this.darMontoTotal(Cuotas) + this.Precio).ToString() ;
+            return CAD;
+        }
+
+        private float Costo
+        {
+            set { this.Precio = value; }
+            get { return this.Precio; }
+        }
+
+        public bool esMasBaratoQue(CPaquete Nuevo)
+        {
+            if (this.Precio < Nuevo.Precio)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
---
+++ Parcial/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir impuesto
+            float IMP;
+            short CUOT;
+
+            Console.WriteLine("INGRESE VALOR DEL IMPUESTO: ");
+            IMP = float.Parse(Console.ReadLine());
+            CPaquete.setIMPUESTO(IMP);
+
+            //crear instancia
+            CPaquete NUEVO;
+
+            string DET;
+            short PAQ;
+            
+
+
+            Console.WriteLine("INGRESE NUMERO DE PAQUETE: ");
+            PAQ = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("INGRESE DETALLE DEL PAQUETE: ");
+            DET = Console.ReadLine();
+
+            NUEVO = new CPaquete(PAQ, DET);
+
+            Console.WriteLine("ESCRIBA PRECIO DEL PAQUETE: ");
+
+            NUEVO.setPrecio(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("INGRESE CANTIDAD DE CUOTAS: ");
+            CUOT = short.Parse(Console.ReadLine());
+            NUEVO.setCuotas(CUOT);
+            
+
+
+            //informar datos recientes
+
+             Console.WriteLine(NUEVO.ToString());
+            Console.ReadLine();
+
+            
+
+
+            //crear instancia
+            CPaquete NUEVO2;
+
+
+            Console.WriteLine("INGRESE NUMERO DE PAQUETE: ");
+            PAQ = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("INGRESE DETALLE DEL PAQUETE: ");
+            DET = Console.ReadLine();
+
+            NUEVO2 = new CPaquete(PAQ, DET);
+
+            Console.WriteLine("ESCRIBA PRECIO DEL PAQUETE: ");
+
+            NUEVO2.setPrecio(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("INGRESE CANTIDAD DE CUOTAS: ");
+            CUOT = short.Parse(Console.ReadLine());
+            NUEVO2.setCuotas(CUOT);
+
+
+
+            //informar datos recientes
+
+            Console.WriteLine(NUEVO2.ToString());
+            Console.ReadLine();
+
+            if (NUEVO.esMasBaratoQue(NUEVO2) == true)
+
+                Console.WriteLine("EL PRIMER PAQUETE ES MAS BARATO. ");
+
+            else
+                Console.WriteLine("EL SEGUNDO PAQUETE ES MAS BARATO. ");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
